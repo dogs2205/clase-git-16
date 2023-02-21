5b10d5e43008489943e743db834e9c8ce76b5db3
NICOLAS_PUERTO <puertonicolas98@gmail.com>
2023-02-20 22:13:34 -0500

    agrege espacios claves

--- script.py
+++ script.py
@@ -3,5 +3,9 @@ print("hola")
 #ls es lista como esxplorer
 
 #ls-la es 
-#git init iniciar git
-#git branch -m main
+#git init (iniciar git
+#git branch -m main (cambiar racismo
+#git add script.py (avanzar estado)
+#git status (ver estado actual)
+#Agregar commit (git commit -m "commint inicial")
+# git log logear
