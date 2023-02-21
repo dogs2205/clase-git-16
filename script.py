754b48cf60fdcc4c06498dcf0e3c1596b901cd15
NICOLAS_PUERTO <puertonicolas98@gmail.com>
2023-02-20 22:36:20 -0500

    Agrego clase persona pero no guardo modulo

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
-from modulo import suma, resta
+from modulo import suma, resta, Persona
 
 print("hola")
 
 print(f"la suma de 1+2={suma(1,2)}")
 
 print(f"la resta de 2-1={resta(2,1)}")
+
+persona_1=Persona()
 #ls es lista como esxplorer
 
 #ls-la es 
